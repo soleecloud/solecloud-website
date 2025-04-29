92c775c4c6ee1df2e7d93dcf48c163b331e06850
SoleCloud <soleecloud@gmail.com>
2025-04-29 13:26:54 -0400

    Fix: Aggressive build fixes - update vercel.json, create placeholder _document.tsx, and modify build script

---
+++ app/_document.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+// This is a placeholder file to replace any cached version
+// since Next.js 13+ app directory doesn't use _document.tsx
+// The actual font loading is in pages/_document.tsx
+
+export default function DocumentPlaceholder() {
+  return null;
+}
